4f51b521e921bfcafa933ca5afbd2e4ad0dd342b
Tina Nguyen <nguyentina2005@yahoo.com>
2017-07-23 15:17:54 -0400

    works
    update read me and code is working.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # bamazon
+An inventory system of an online storefront with the MySQL. The app will take in orders from customers and deplete stock from the store's inventory. 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,7 +23,7 @@ function displayProducts() {
     connection.query("SELECT * FROM products_list", function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + "  " + res[i].product_name + "  " + res[i].department_name + " " + res[i].price);
+            console.log("ItemId:  "+res[i].item_id + " | " + "Name: " + res[i].product_name + "  |   " + "Price : " +"$"+ res[i].price);
         }
     });
     customerBuy();
@@ -69,7 +69,64 @@ function customerBuy() {
                 }
                     }
                 ]).then(function (answer) {
-            console.log("works");
+            updateQuantity(answer.productId, answer.quantity);
         })
     });
+}
+
+function updateQuantity(id, amount) {
+    //making sure the right values was inherit
+    //console.log(id + " " + amount);
+    connection.query("Select * from products_list Where ?", [{
+            //selecting only the item information that the user picked
+            item_id: id
+                     }],
+        function (err, res) {
+            if (err) throw err;
+
+            //check if only the picked item data was retrive.
+            //console.log(res);
+
+            var newQuantity = res[0].stock_quantity - amount;
+            var itemId = res[0].item_id;
+            var name = res[0].product_name;
+            var price = res[0].price;
+            var cost = res[0].price * amount;
+            
+            //making sure the varaiable data was correct    
+            //console.log(itemId); 
+            //console.log(newquantity);
+            //console.log(cost);
+        
+            connection.query("Update products_list Set ? Where ?", [
+                {
+                    stock_quantity: newQuantity
+                    }, {
+                    item_id: itemId
+                        }
+            ], function (err, res) {
+                if (err) throw err;
+                console.log("--------------------");
+                console.log("Bamazon Receipt");
+                console.log("   ");
+                console.log(amount +" "+ name + " cost " + price + " each.");
+                console.log("Total amount $" + cost);
+                console.log("--------------------");
+                inquirer.prompt([{
+                    type: "confirm",
+                    message:"Do you want to buy something else?",
+                    name:"confirm",
+                    default:true
+                }]).then(function(answer){
+                //user want to continue shopping 
+                    if(answer.confirm){
+                        console.log("Do you want to make another order");
+                        customerBuy();    
+                    }else{
+                //user done with shopping
+                        console.log("Thank you for shopping at Bamazon. Have a nice day.");
+                    }
+                })
+            })
+        });
 }
